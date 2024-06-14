bad0670c4f5598a42348793a3f9123ea0862b6a6
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-06-14 05:22:35 -0700

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,17 @@ from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+    def __init__(self, *args, **kwargs):
+        if kwags:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def __str__(self):
         name = self.__class__.__name__
