a9e4aa5d3556d009b0bef506b757bab3f746e0b5
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-06-15 02:47:50 -0700

    AirBnB

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """ Quit command to exit the program """
+        return (True)
+
+    def do_EOF(self, line):
+        """ EOF signal to exit the program """
+        print("")
+        return (True)
+
+    def emptyline(self):
+        """ Pass when getting an empty line """
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
