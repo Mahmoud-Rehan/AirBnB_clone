c1dd5dc1271ff62e3846fa6b4ab67552ca2316d1
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-06-15 03:08:31 -0700

    AirBnB

--- console.py
+++ console.py
@@ -2,22 +2,30 @@
 """ HBNBCommand Class Module """
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb) '
-
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
-        return True
-
-    def do_EOF(self, arg):
-        """EOF command to exit the program"""
-        print()
-        return True
+    """
+    HBNBCommand console class
+    """
+    prompt = "(hbnb) "
 
     def emptyline(self):
-        """Do nothing on empty input line"""
+        """
+        Do nothing when an empty line is entered.
+        """
         pass
+
+    def do_EOF(self, arg):
+        """
+        EOF (Ctrl+D) signal to exit the program.
+        """
+        return True
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program.
+        """
+        return True
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
