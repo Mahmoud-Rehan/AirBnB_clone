19f6d03f51e408b1298976ed8849b5b236d3b73d
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-06-16 05:32:20 -0700

    AirBnB

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """ HBNBCommand Class Module """
 import cmd
 from models.base_model import BaseModel
-
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """ HBNBCommand Class """
@@ -36,6 +36,26 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **")
         else:
             print("** class name missing **")
+
+    def do_show(self, line):
+        """ Show the string representation of an instance
+        based on <Class name> and <Id> """
+        lines = line.split()
+
+        if not lines:
+            print("** class name missing **")
+        elif lines[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        elif len(lines) < 2:
+            print("** instance id missing **")
+        else:
+            key = f"{lines[0]}.{lines[1]}"
+
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                print(storage.all()[key])
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
