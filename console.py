a3252ce650bf4a5907787036f09a794ef2db5a54
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-06-25 07:41:50 -0700

    AirBnB

--- console.py
+++ console.py
@@ -3,12 +3,11 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
-from models.user import User
-from models.state import state
+from models.state import State
 from models.city import city
-from models.amenity import amenity
-from models.place import place
-from models.review import review
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
 
 
